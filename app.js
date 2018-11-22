19493c301348371a4d6c4f4e199f8d11fe202c96
Axel Vestberg <axel.vestberg@gmail.com>
2018-11-22 20:10:50 +0100

    trying out hosting on google cloud platform to test real webapp

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require("express");
+
+var app = express();
+
+app.use(express.static('public'));
+
+if (module === require.main) {
+    // [START server]
+    // Start the server
+    const server = app.listen(process.env.PORT || 8080, () => {
+      const port = server.address().port;
+      console.log(`App listening on port ${port}`);
+    });
+    // [END server]
+  }
+  
+  module.exports = app;
